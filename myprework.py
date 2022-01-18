fba1a5d90c5367f2eba5df357966044d824e6009
maamerio24 <michaelamerio15@gmail.com>
2022-01-17 22:53:38 -0800

    first commit

---
+++ myprework.py
@@ -0,0 +1,46 @@
+#Question 1:
+#Write a function to print "hello.USERNAME!" USERNAME is the input of the function. The first line of the code has been defined as below.
+
+def hello_name(user_name):
+    print("hello." + user_name)
+
+hello_name("USERNAME")
+
+#Question 2:
+#Write a python function, first_odds that prints the odd numbers from 1-100 and returns nothing
+
+def first_odds():
+    for number in range(1,101):
+        if number % 2 == 1:
+            print(number)
+        else:
+            continue
+first_odds()
+
+#Question 3
+#Please write a Python function, max_num_in_list to return the max number of a given list. The first line of code has been defined as below.
+
+def max_num_in_list(a_list):
+    max = a_list[0]
+    for a in a_list:
+        if a > max:
+            max = a
+    return max
+
+print(max_num_in_list([4, 1, -3, 0, 15]))
+
+#Question 4 
+#Write a function to return if the given year is a leap year. A leap year is divisible by 4, but not divisible by 100, unless it is also divisible by 400. The return should be boolean Type (true/false).
+
+def is_leap_year(a_year):
+    leap = False
+    if (a_year % 4 == 0 and a_year % 100 != 0) or a_year % 400 == 0:
+        leap = True
+    return leap
+
+print(is_leap_year(2015))
+
+#Question 5
+#Write a function to check to see if all numbers in list are consecutive numbers. For example, [2,3,4,5,6,7] are consecutive numbers, but [1,2,4,5] are not consecutive numbers. The return type should be boolean Type.
+
+def is_consecutive(a_list):
